7f5862d56409e022f5d4e34a760b58501ef68edc
Ajay <ajay3303attri@gmail.com>
2020-08-25 17:28:03 +0530

    Initial Commit

---
+++ CustomCell_TableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomCell_TableView
+//
+//  Created by Ajay on 25/08/20.
+//  Copyright © 2020 Ajay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
